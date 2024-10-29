8bd1ece8dd540718b72aa25cbd6e9bfad3213f46
IanSantosOS <iann.silva2006@gmail.com>
2024-10-29 11:09:09 -0300

    small changes

--- models/User.js
+++ models/User.js
@@ -12,16 +12,25 @@ const User = sequelize.define('user', {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
+        set(value) {
+            this.setDataValue('name', value);
+        },
     },
     username: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        set(value) {
+            this.setDataValue('username', value);
+        },
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        set(value) {
+            this.setDataValue('email', value);
+        },
     },
     password: {
         type: DataTypes.STRING,
@@ -31,8 +40,12 @@ const User = sequelize.define('user', {
 
 // Hook para hash da senha antes de salvar o usuário no banco de dados
 User.beforeCreate(async (user, options) => {
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+    user.password = await hash(user.password);
 });
+
+async function hash(word) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(word, salt);
+}
 
 module.exports = User;
--- routes/router.js
+++ routes/router.js
@@ -16,24 +16,44 @@ router.get('/users', async (req, res) => {
 });
 
 router.post('/login', async ({ body }, res) => {
-    console.log(body)
     const user = await User.findOne({ where: { username: body.username } });
-    const passVerified = await bcrypt.compare(body.password, user.password);
+    let userVerified = false;
+    if (user) {
+        userVerified = await bcrypt.compare(body.password, user.password);
+    }
 
-    res.json({ data: passVerified });
+    res.json({ data: userVerified });
 });
 
 // router.post('/logout', (req, res) => {});
 
 router.post('/register', async ({ body: user }, res) => {
-    await User.create(user);
-    res.json(user);
+    if (user.name.trim() === "" || user.username.trim() === "" || user.email.trim() === "" || user.password.trim() === "" ) {
+        res.json({ data: false });
+    } else if (user.password === user.passwordVerify) {
+        await User.create(user);
+        res.json({ data: user });
+    } else {
+        res.json({ data: false });
+    }
 });
 
-router.put('/update/user/:id', async (req, res) => {
-    const { id } = req.params;
-    await User.update(req.body, { where: { id } });
-    res.json({});
+router.put('/update/user/:id', async ({ body, params }, res) => {
+    const { id } = params;
+    const user = await User.findOne({ where: { id } });
+
+    if (body?.name && body.name.trim() !== "") {
+        user.name = body.name;
+    }
+    if (body?.username && body.username.trim() !== "") {
+        user.username = body.username;
+    }
+    if (body?.email && body.email.trim() !== "") {
+        user.email = body.email;
+    }
+
+    await user.save();
+    res.json({ data: true });
 });
 
 router.delete('/delete/user/:id', async (req, res) => {
