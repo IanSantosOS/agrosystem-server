e9308ef1f40df85eb9df685a49b1e541f192fb23
IanSantosOS <iann.silva2006@gmail.com>
2024-11-23 19:16:32 -0300

    docs stuff

--- swagger.js
+++ swagger.js
@@ -1,16 +1,22 @@
-const swaggerAutogen = require('swagger-autogen')();
 require('dotenv').config();
+const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-  info: {
-    title: 'API Agrosystem',
-    description: 'Documentação gerada automaticamente pelo Swagger',
-  },
-  host: process.env.SERVER_HOST + ':' + process.env.SERVER_PORT,
-  schemes: ['http'],
+    info: {
+        title: 'API Agrosystem',
+        description: 'Documentação gerada automaticamente pelo Swagger',
+    },
+    host: process.env.SERVER_HOST + ':' + process.env.SERVER_PORT,
+    schemes: ['http', 'https'],
 };
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./app.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc)
+    .then(() => {
+        console.log(`Swagger documentation generated at: ${outputFile}`);
+    })
+    .catch((error) => {
+        console.error('Failed to generate Swagger documentation:', error);
+    });
